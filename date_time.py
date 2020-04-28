f09cf9221bead87b77d943772fe7b90a2278b36c
rajindergandhi <Sanvi@2304>
2020-04-28 19:00:58 +0530

    Date time format

---
+++ date_time.py
@@ -0,0 +1,20 @@
+import datetime
+
+x= datetime.datetime.now()
+print(x)
+
+print (x.year)
+print(x.month)
+print(x.day)
+print(x.hour)
+print(x.second)
+print(x.microsecond)
+print(x.strftime("%B"))
+print(x.strftime("%b"))
+print(x.strftime("%A"))
+print(x.strftime("%a"))
+
+print(x.strftime("%H"))
+print(x.strftime("%I"))
+print(x.strftime("%p"))
+print(x.strftime("%X"))
