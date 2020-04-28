07089d0e2bf86ad26f311c14206e028d70301559
rajindergandhi <Sanvi@2304>
2020-04-28 19:01:18 +0530

    Anagram program in python

---
+++ Anagram.py
@@ -0,0 +1,8 @@
+a= input("Enter the first string")
+b= input("Enter the second string")
+if sorted(a)==sorted(b):
+    print("Then string is Anagram")
+else:
+    print("The string is not Anagram")
+
+## Earth Heart , Traingle Integral , Listen silent
